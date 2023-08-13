de4907e1a10dc0ad07be640cb11ffcb91b05b5d8
Joscha Loos <j.loos@posteo.net>
2023-08-13 16:09:56 +0200

    build: flake nix

--- flake.nix
+++ flake.nix
@@ -10,30 +10,34 @@
       pkgs = import nixpkgs { inherit system; };
 
       version = "0.2.0";
+
+      build = cmd: pkgs.buildNpmPackage {
+        inherit version;
+        pname = "kitingkeys";
+        src = ./.;
+        npmDepsHash = "sha256-nwtvcrUTACVZPbwYlSJhAEqRPgnEFy7D5PuDNs0HoLc=";
+
+        PUPPETEER_SKIP_DOWNLOAD = true;
+        browser = "firefox"; # env var for webpack
+
+        configurePhase = ''
+          sed -i '3i\  "version": "${version}",' package.json
+        '';
+
+        buildPhase = ''
+          npm install
+          npm run ${cmd}
+        '';
+
+        installPhase = ''
+          mkdir $out
+          cp -r ./dist $out/dist
+        '';
+      };
     in {
-      defaultPackage.${system} =
-        pkgs.buildNpmPackage {
-          inherit version;
-          pname = "kitingkeys";
-          src = ./.;
-          npmDepsHash = "sha256-nwtvcrUTACVZPbwYlSJhAEqRPgnEFy7D5PuDNs0HoLc=";
-
-          PUPPETEER_SKIP_DOWNLOAD = true;
-          browser = "firefox"; # env var for webpack
-
-          configurePhase = ''
-            sed -i '3i\  "version": "${version}",' package.json
-          '';
-
-          buildPhase = ''
-            npm install
-            npm run build
-          '';
-
-          installPhase = ''
-            mkdir $out
-            cp -r ./dist $out/dist
-          '';
-        };
+      packages.${system}= {
+        default = build "build";
+        fast = build "build:dev";
+      };
     };
 }
