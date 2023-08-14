81a47096a7a98b8832fd66237cb110cb57663ab5
Joscha Loos <j.loos@posteo.net>
2023-08-14 22:54:15 +0200

    fix: version number conflict

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-      version = "0.3.2";
+      version = "0.3.3";
 
       build = cmd: pkgs.buildNpmPackage {
         inherit version;
